a83255badc79455f0f5a7bae292316d60d1e2acf
Devv1507 <davide.valencia.v@gmail.com>
2025-04-05 14:21:05 -0500

    Game model implementation builded

--- src/main/java/com/example/crapsgame/models/Game.java
+++ src/main/java/com/example/crapsgame/models/Game.java
@@ -1,4 +1,78 @@
 package com.example.crapsgame.models;
 
+
+/***
+ * This represents the state of the CrapsGame.
+ * @author David Valencia
+ * @author Santiago Guerrero
+ * @author Juan Pablo Escamilla
+ */
 public class Game {
+    private int point;
+    private int wonGames;
+    private int lossGames;
+
+
+    public Game() {
+        this.point = 0;
+        this.wonGames = 0;
+        this.lossGames = 0;
+    }
+
+
+    /**
+     * Evaluates each round of the Craps Game.
+     * Each iteration change the attributes of this class, representing the state of the game.
+     * @param shotValue - sum of roll values of dice1 and dice2
+     */
+    public void evaluateRoll(int shotValue) {
+        if (this.point == 0) {
+            if (shotValue == 7 || shotValue == 11) {
+                this.wonGames++;
+            }
+            else if (shotValue == 2 || shotValue == 3 || shotValue == 12) {
+                this.lossGames++;
+            }
+            else {
+                this.point = shotValue;
+            }
+        } else {
+            if (shotValue == this.point) {
+                this.wonGames++;
+            }
+            else if (shotValue == 7) {
+                this.lossGames--;
+            }
+            else {
+                this.point = shotValue;
+            }
+        }
+    }
+
+
+    /**
+     * Retrieve the won games.
+     * @return the won game.
+     */
+    public int getWonGames() {
+        return this.wonGames;
+    }
+
+
+    /**
+     * Retrieve the loss games.
+     * @return the loss game.
+     */
+    public int getLossGames() {
+        return this.lossGames;
+    }
+
+
+    /**
+     * Retrieve the current point.
+     * @return the value of the point.
+     */
+    public int getPoint() {
+        return this.point;
+    }
 }

662ca101e204a9d9e9db39d5b45ad8173a49ba62
Devv1507 <davide.valencia.v@gmail.com>
2025-04-05 22:56:41 -0500

    Removed of old unnecessary commented lines

--- src/main/java/com/example/crapsgame/Main.java
+++ src/main/java/com/example/crapsgame/Main.java
@@ -11,47 +11,6 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         WelcomeView welcomeView = WelcomeView.getInstance();
         welcomeView.show();
-
-        /*
-        Button button = new Button("Event Test");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello, I am a Button Event");
-                System.out.println("Type:" + actionEvent.getEventType());
-                System.out.println("Source:" + actionEvent.getSource());
-                System.out.println("Target:" + actionEvent.getTarget());
-            }
-        });
-
-        Label label = new Label("Hello FOPE");
-        label.setOnMouseClicked((new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("Hello, I am a Label Event");
-                System.out.println("Type:" + mouseEvent.getEventType());
-                System.out.println("Source:" + mouseEvent.getSource());
-                System.out.println("Target:" + mouseEvent.getTarget());
-            }
-        }));
-
-        VBox vbox = new VBox();
-        vbox.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("Hello, I am a VBox Event");
-                System.out.println("Type:" + mouseEvent.getEventType());
-                System.out.println("Source:" + mouseEvent.getSource());
-                System.out.println("Target:" + mouseEvent.getTarget());
-            }
-        });
-
-        vbox.getChildren().add(label);
-        vbox.getChildren().add(button);
-        Scene scene = new Scene(vbox, 200, 200);
-        stage.setScene(scene);
-        stage.show();
-        */
     }
 
     public static void main(String[] args) {

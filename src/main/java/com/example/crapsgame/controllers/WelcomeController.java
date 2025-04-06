447dea74ad0f298e1c749eb8a81cd0a4068b7755
Devv1507 <davide.valencia.v@gmail.com>
2025-04-05 23:03:31 -0500

    Removed unnecessary files

--- src/main/java/com/example/crapsgame/controllers/WelcomeController.java
+++ src/main/java/com/example/crapsgame/controllers/WelcomeController.java
@@ -1,14 +1,11 @@
 package com.example.crapsgame.controllers;
 
-import com.example.crapsgame.models.AlertBox;
 import com.example.crapsgame.models.Player;
 import com.example.crapsgame.views.GameView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 
